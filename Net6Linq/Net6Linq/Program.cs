f0f71b11d95636a57dde36812a69e0523063e223
Sepehr-Glpz <57174194+Sepehr-Glpz@users.noreply.github.com>
2022-02-04 21:55:03 +0330

    Initial Create

---
+++ Net6Linq/Net6Linq/Program.cs
@@ -0,0 +1,148 @@
+﻿//Hi There!
+using System;
+using System.Collections.Generic;
+using System.Collections;
+//Dont forget to import the linq namespace first so you can actually use the extension methods!
+using System.Linq;
+
+
+// #1 Zip()
+
+//suppose we have 2 collections
+
+int[] numberArray = new int[] { 1, 2, 3, 4 };
+
+string[] stringArray = new string[] { "first", "second", "third", "forth" };
+
+// now what if we wanted to put each elements of the 2 arrays into a grouping next to each other?
+// something like: (1,"first") , (2,"second), (3,"third) ?
+// We can acheive that using the new Linq Zip() extention method!
+// the method returns a tuple of the joined values.
+
+IEnumerable<(int number, string name)> zipedArray = numberArray.Zip(stringArray);
+
+var valuesArray = zipedArray.ToArray();
+
+//You can even deconstruct the joined values in a foreach loop!
+foreach (var (number, name) in zipedArray)
+{
+    Console.WriteLine($"({number},{name})");
+}
+
+// Bonus! you can even zip 3 collections together if you would like!
+
+double[] percents = new double[] { 0.1D, 0.2D, 0.3D };
+
+var tripleZippedArray = numberArray.Zip(stringArray, percents).ToArray();
+
+Console.WriteLine("Bonus:");
+foreach (var (number, name, percent) in tripleZippedArray)
+{
+    Console.WriteLine($"({number},{name},{percent})");
+}
+
+Console.ReadLine();
+
+
+// #2 Chunk()
+
+// susppose we have a collection
+
+IEnumerable<int> veryBigList = Enumerable.Range(0, 21420);
+
+//As you might notice, this collection is very large and one might say they would prefer
+//to split it up into smaller pieces and perform multiple batch operations on it
+//for example a bulk database call that only accepts smaller ranges of data
+//we can split up this collection with the new Linq Chunk() Mehtod!
+
+IEnumerable<int[]> smallerLists = veryBigList.Chunk(500);
+
+//As you can see, the smaller collections are gathered in new arrays NOT enumerables!
+
+//NOTICE! for simplicity sake i will override our example with a smaller array so it is easier to read the output
+// Feel free to delete the next 2 lines in your own testing envoironment ^^
+veryBigList = Enumerable.Range(0, 32);
+smallerLists = veryBigList.Chunk(8);
+//
+
+foreach (var smallerList in smallerLists)
+{
+    int lenght = smallerList.Length;
+    for (int index = 0; index < lenght; index++)
+    {
+        Console.Write($"{smallerList[index]}-");
+    }
+    Console.WriteLine("\nJust passed a chunk!\n");
+}
+
+Console.ReadLine();
+
+// #3 new *By methods improvments!
+
+// The Distinct() extension Method now has a DistinctBy() version which allows you to specify a sub set of a value
+// to use during comparison!
+
+var items = new[]
+{
+    new { Name = "James", Age = 18 },
+    new { Name = "John", Age = 18 },
+    new { Name = "John", Age = 30 },
+};
+
+var distinctByNameItems = items.DistinctBy(d => d.Name).ToArray();
+var distinctByAgeItems = items.DistinctBy(d => d.Age).ToArray();
+
+Console.WriteLine("Distinct by Name:");
+foreach(var item in distinctByNameItems)
+{
+    Console.WriteLine($"Name: {item.Name} ,Age: {item.Age} ");
+}
+Console.WriteLine("\nDistinct by Age:");
+foreach (var item in distinctByAgeItems)
+{
+    Console.WriteLine($"Name: {item.Name} ,Age: {item.Age} ");
+}
+
+Console.ReadLine();
+
+// The ExceptBy() Method is also new, you can compare and remove items from a collection based on
+// keys provided from another collection.
+
+var androids = new[]
+{
+    new { Name = "2B", Age = 18 },
+    new { Name = "9S", Age = 18 },
+    new { Name = "A2", Age = 30 },
+    new { Name = "8B", Age = 25 },
+};
+
+var traitorNames = new string[]
+{
+    "A2",
+    "8B",
+};
+
+//This will remove all items with the same name as the provided names
+var exceptTraitors = androids.ExceptBy(traitorNames,s => s.Name); 
+
+Console.WriteLine("The androids whose name were in traitors were removed:");
+foreach(var android in exceptTraitors)
+{
+    Console.WriteLine($"Name: {android.Name} , Age: {android.Age} ");
+}
+Console.ReadLine();
+
+// We also have the new IntersectBy() method!
+// 
+
+var dangerousPeople = new[]
+{
+    new { Name = "My Neighbour", DangerLevel = 1000 },
+    new { Name = "Local Cat", DangerLevel = 1800 },
+    new { Name = "Angry Customer", DangerLevel = 900 },
+};
+
+var oldDangerousRecord = new[]
+{
+    new { Name = ""}
+};
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Net6Linq
-Some new Linq Apis in .NET 6!
+# .Net 6 Linq
+Showcasing some new Linq Apis in .NET 6!
+
+Here we will take a look at some of the new provided Linq apis which were introduced in .NET 6.
+
+by:
+[Sepehr golpazir](https://www.linkedin.com/in/sepehr-golpazir-161559197/)
